51385517de1c933cd0b238c2aa6f1f783e4cb1f5
rosaspace <rosaspace@126.com>
2024-09-23 15:09:44 -0500

    support redis

--- capstone/settings.py
+++ capstone/settings.py
@@ -51,7 +51,9 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("redis", 6379)],
+            # "hosts": [("redis", 6379)],  # docker
+            # "hosts": [('127.0.0.1', 6379)],  # localhost WSL
+            "hosts": [os.environ.get('REDIS_URL')],
         },
     },
 }
